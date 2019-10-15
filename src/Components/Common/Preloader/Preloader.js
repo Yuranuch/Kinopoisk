316c424d19c48fdfee6c6074eb89a4a90ecd7e72
Yuranuch <yura477573@mail.ru>
2019-10-16 01:50:38 +0300

    some redesign

--- src/Components/Common/Preloader/Preloader.js
+++ src/Components/Common/Preloader/Preloader.js
@@ -1,5 +1,5 @@
 import React from "react"
-import preloader from "../../../assets/images/preloader.gif"
+import preloader from "../../../assets/images/739.gif"
 import styles from "./Preloader.module.css"
 
 function Preloader() {
