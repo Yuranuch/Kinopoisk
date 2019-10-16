faad45d42d60c526e6f45d7fd4f4a53183bd2749
Yuranuch <yura477573@mail.ru>
2019-10-16 14:35:19 +0300

    use hashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react"
-import {BrowserRouter, Route} from "react-router-dom"
+import {HashRouter, Route} from "react-router-dom"
 import "./App.css"
 import DetailsPage from "./Components/DetailsPage/DetailsPage"
 import SearchPage from "./Components/SearchPage/SearchPage"
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className="wrapper">
                 <div className="content">
                     <Route exact path="/" component={SearchPage}/>
                     <Route path="/detail/:filmId?" component={DetailsPage}/>
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
